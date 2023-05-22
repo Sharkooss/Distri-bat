112a93a5f81f0cef84eb8690ed7bba99cc45e7d3
Sharkooss <lnectoux04@gmail.com>
2023-05-22 11:48:07 +0200

    debut

--- index.php
+++ index.php
@@ -4,13 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Distri-Bat</title>
 </head>
 <header>
     <?php include 'php\header.php'; ?>
-    
+    <div class="banniere-block">
+        <div class="banniere-texte">
+            <div class="banniere-titre">DISTRI-BAT sinistres</div>
+            <div class="banniere-soustitre">Isolation - peinture - facade</div>
+        </div>
+    </div>
 </header>      
 <body>
-    
+    <div class="presentation-container">
+        <div class="presentation-block-gris"></div>
+        <div class="presentation-texte1"></div>
+        <div class="presentation-texte2"></div>
+        <div class="presentation-boutton-devis"></div>
+    </div>    
 </body>
 </html>
