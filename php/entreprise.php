d1ce0e765109fcc9d2ff704b1b0a08d29dfb6ba6
Petre Charlie <charliepetre2004@gmail.com>
2023-05-23 13:51:37 +0200

    22/05

---
+++ php/entreprise.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/entreprise.css">
+    <link rel="stylesheet" href="/css/footer.css">
+    <link rel="stylesheet" href="/css/header.css">
+    <title>Document</title>
+</head>
+<header>
+    <?php include 'header.php'; ?>    
+</header>   
+<body>
+
+<div class="containerentreprise">
+    <div class="historique">
+        <img class="imghistorique" src="/photo/entreprise/historique.jpg" alt="histoire entreprise image">
+        <div class="parahistorique"></div>
+    </div>
+    <div class="equipe">
+        <div class="paraequipe"></div>
+    </div>
+    <div class="choisir">
+        <div class="parachoisir"></div>
+    </div>
+    <div class="certification">
+        <div class="paracertification"></div>
+    </div>
+</div>
+
+
+</body>
+
+</html>
