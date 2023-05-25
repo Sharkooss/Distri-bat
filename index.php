218eb8f4f6954dbc4a6a3a21a11324feb092e875
Sharkooss <lnectoux04@gmail.com>
2023-05-25 10:46:10 +0200

    jai finit le contact-transi

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="stylesheet" href="/css/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Distri-Bat</title>
 </head>
 <header>
@@ -14,12 +15,105 @@
 </header>      
 <body>
     <div class="presentation-container">
-        <div class="presentation-block-gris"></div>
-        <div class="presentation-texte1"></div>
-        <div class="presentation-texte2"></div>
-        <div class="presentation-boutton-devis"></div>
+        <div class="presentation-flex">
+            <div class="presentation-block-gris">
+                <div class="block-gris">Ravalement de facades</div>
+                <div class="block-gris">isolation thermique <br>par l’exterieur</div>
+                <div class="block-gris">Rénovation embelissement interieur/exterieur</div>
+            </div>
+            <div class="presentation-texte1">
+            Spécialisée dans les travaux de <b>rénovation de façades</b> depuis plus de 30 ans, notre entreprise dispose
+            d'un panel de savoir-faire pour vous accompagner en continu durant vos <b>travaux de rénovation.</b> 
+            Notre objectif ? Vous apporter notre expertise afin de vous proposer une offre, des <b>conseils personnalisés</b>
+            répondant à vos besoins. <b>DISTRI-BAT SINISTRES</b> est implantée dans la zone industrielle de Gigean et nos équipes 
+            interviennent sur tous les départements du Languedoc-Roussillon.
+            </div>
+            <div class="presentation-texte2">
+            Rénovation de façades <a class="b">//</a> Traitement de fissures <a class="b">//</a> Isolation thermique par 
+            l'extérieur (ITE) <a class="b">//</a> Travaux intérieurs (peinture, papier-peint, décoration, carrelage et revêtement de sol) ...
+            </div>
+            <div class="presentation-boutton-devis">
+                <div class="button-devis">Demander Un Devis</div>
+            </div>
+        </div>
     </div>
+    <div class="index-caroussel">
+        <div class="slider">
+            <div class="slides">
+                <div class="slide"><img src="/photo/frontignan.jpg" alt=""></div>
+                <div class="slide"><img src="/photo/facadei3.jpg" alt=""></div>
+                <div class="slide"><img src="/photo/interieurlattes.jpg" alt=""></div>
+
+            </div>
+        </div>
+    </div>
+    <div class="prestation-container">
+        <div class="prestation-title"><u>Nos Prestations</u></div>
+        <div class="prestations-block">            
+            <div class="prestation-cartes-1">
+                <div class="prestation-cartes-face">
+                    <div class="prestation-image"><img src="/photo/prestation/facade.jpg" alt=""></div>
+                    <div class="prestation-texte">Expert en façade</div>
+                </div>
+                <div class="prestation-cartes-back">
+                    <p>Distri-Bat Sinistres vous apporte conseils et solutions adaptés dans vos projets: 
+                    techniques de <b>ravalement, d'imperméabilisation et de rénovation de façades.</b></p>
+                </div>
+            </div>
+            <div class="prestation-cartes-2">
+                <div class="prestation-cartes-face">
+                    <div class="prestation-image"><img src="/photo/prestation/fissures.jpg" alt=""></div>
+                    <div class="prestation-texte">traitement des fissures</div>
+                </div>
+                <div class="prestation-cartes-back">
+                    <p>Notre expertise nous permet de réaliser l'ensemble des travaux pour <b>traiter</b> tout type 
+                        de <b>fissures</b> et éviter l'aggravation des dommages sur votre <b>façade</b>.</p>
+                </div>
+            </div>
+            <div class="prestation-cartes-3">
+                <div class="prestation-cartes-face">
+                    <div class="prestation-image"><img src="/photo/prestation/isolation.jpg" alt=""></div>
+                    <div class="prestation-texte">Isolation Thermique<br>par l’exterieur</div>
+                </div>
+                <div class="prestation-cartes-back">
+                    <p>Qualifiée RGE QUALIBAT, Distri-Bat Sinistres vous accompagne dans <b>l'étude de vos projets d'ITE.</b></p>
+                </div>
+            </div>
+            
+            <div class="prestation-cartes-4">
+                <div class="prestation-cartes-face">
+                    <div class="prestation-image"><img src="/photo/prestation/interieur.jpg" alt=""></div>
+                    <div class="prestation-texte">Travaux intérieurs</div>
+                </div>
+                <div class="prestation-cartes-back">
+                    <p>Nos différents corps de métier nous permettent d'intervenir dans <b>l'amélioration de votre habitat et projets de décoration</b></p>
+                </div>
+            </div>          
+        </div>
+    </div>
+
+    <div class="transi-contact-container">
+        <div class="transi-contact-gauche">
+            <div class="transi-contact-texteG1">
+                <p>Vous souhaitez démarrer un projet de <span class="bleu">ravalement façades</span>, d'<span class="bleu">isolation thermique</span> par l'extérieur ou de <span class="bleu">travaux intérieurs ?</span></p>
+            </div>
+            <div class="transi-contact-texteG2">
+                <p>Vous faites face à un <span class="bleu">sinistre</span> ?</p>
+            </div>
+        </div>
+        <div class="transi-contact-droite">
+            <div class="transi-contact-texteD">
+                <p>Contactez-nous <br>pour obtenir un devis</p>
+            </div>
+            <div class="transi-contact-btn-block">
+                <div class="transi-contact-btn">mail</div>
+                <div class="transi-contact-btn">contact</div>
+            </div>
+        </div>
+    </div>
+
+
     
 </body>
-<?php include 'php\footer.php'; ?>
+<!-- <?php include 'php\footer.php'; ?> -->
 </html>
