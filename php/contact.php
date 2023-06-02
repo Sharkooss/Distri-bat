f22b5a2932d9803d276b3f69d2cb266a297ceb70
Petre Charlie <charliepetre2004@gmail.com>
2023-06-02 16:59:48 +0200

    a peu près la moitié des contacts

---
+++ php/contact.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/contact.css">
+    <link rel="stylesheet" href="/css/footer.css">
+    <link rel="stylesheet" href="/css/header.css">
+    <title>Document</title>
+</head>
+
+
+<header>
+    <?php include 'header.php'; ?>    
+</header>
+
+
+<body>
+
+
+<div class="contmom">
+    <div class="containercontacttitle">
+        <h1>Contact</h1>
+    </div>
+
+    <div class="containercontact">
+
+
+    <div class="contactgauche">
+        <h2>rénovation et embellissements intérieurs</h2>
+        <h2>Isolation thermique par <br> l'extérieur</h2>
+        <h2>Ravalement de façades</h2>
+    </div>
+    <div class="paracontact"></div>
+    <div class="contactinfo">
+    <div class="coocontainer"><img src="\photo\contact\phone.svg" alt="phone photo" class="contactsvg"> <h2>06 70 04 55 76</h2></div>
+        <div class="coocontainer"><img src="\photo\contact\at.svg" alt="mail photo" class="contactsvg"> <h2>distribatsinistres@ <br> gmail.com</h2></div>
+
+        <div class="coocontainer"><img src="\photo\contact\location.svg" alt="location photo" class="contactsvg"> <h2>Siège social à Lattes <br> Agence logistique à Poussan</h2></div>
+    </div>
+    <div class="paracontact"></div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.4003355269156!2d3.67300007657708!3d43.47311506405905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6bb6565230a9b%3A0x15ab78633134ae21!2sDISTRI-BAT%20SINISTRES!5e0!3m2!1sen!2sfr!4v1685709156392!5m2!1sen!2sfr" width="800" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="locacontact"></iframe>
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+</html>
