b1ab39265809c3affde31bdaec05e796b00cf180
Sharkooss <lnectoux04@gmail.com>
2023-06-05 09:52:07 +0200

    fintio

--- php/footer.php
+++ php/footer.php
@@ -31,16 +31,35 @@
                 <div class="footer-underline locaux-u"></div>
             </div>
             <div class="siege-social">
-                <div class="siege-social-left">
-                    <img src="../photo/logo/home.svg" alt="" class="home-img">
-                    <div class="siege-social-txt">
-                        <div class="siege-social-title"><h2>Siège social</h2></div>
-                        <div class="siege-social-lieu"><p>16 rue des Érables, 34970 Lattes</p></div>
+                
+                <div class="siege-social-top">
+                    <div class="siege-social-topleft">
+                        <img src="../photo/logo/home.svg" alt="" class="home-img">
+                        <div class="siege-social-txt">
+                            <h2>Siège social</h2>
+                            <p>16 rue des Érables, 34970 Lattes</p>
+                        </div>
                     </div>
+
+                    <div class="siege-social-topright">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d180.68299699336902!2d3.912706055231308!3d43.56638436233913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6baac22356ce1%3A0xf1a3ed331e75ad12!2s16%20Rue%20des%20%C3%89rables%2C%2034970%20Lattes!5e0!3m2!1sfr!2sfr!4v1685949066065!5m2!1sfr!2sfr"style="border:1px solid #373B4D ;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>                        
                 </div>
-                <div class="siege-social-right">
-                    
+                <div class="siege-social-bot">
+
+                    <div class="siege-social-botleft">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11581.554335626044!2d3.672957164001465!3d43.47336034789388!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6bb6565230a9b%3A0x15ab78633134ae21!2sDISTRI-BAT%20SINISTRES!5e0!3m2!1sfr!2sfr!4v1685949255076!5m2!1sfr!2sfr" style="border:1px solid #373B4D ;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                    <div class="siege-social-botleft">
+                        <img src="../photo/logo/home.svg" alt="" class="home-img">
+                        <div class="siege-social-txt">
+                            <h2>Agence logistique</h2>
+                            <p>304 Av. des Lauriers, 34560 Poussan</p>
+                        </div>
+                    </div>
+
                 </div>
+                
             </div>
             <div class="agence-logistique"></div>
         </div>
@@ -49,8 +68,11 @@
     <div class="overflow-devis">
         <div class="menufooterdevis" id="menufooterdevis">
             <div class="footer-title devis-t">
-                <h2>Demande de Devis</h2>
+                <h2>politique de confidentialité</h2>
                 <div class="footer-underline devis-u"></div>
+            </div>
+            <div class="mentions-legales">
+                <h2>Mentions légales</h2>
             </div>
         </div>
     </div>
