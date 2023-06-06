1247ee97a6d6d9c796da7fa085fadefcf4ed7d1b
Sharkooss <lnectoux04@gmail.com>
2023-06-06 14:48:54 +0200

    index animate

--- js/index.js
+++ js/index.js
@@ -14,23 +14,76 @@ window.addEventListener('DOMContentLoaded', function() {
       });
 
 window.addEventListener('scroll', function() {
-  var gerantelement = document.querySelector('.gerant-element');
-  var gerantelementPosition = gerantelement.getBoundingClientRect().top;
+  
+  var windowHeight = window.innerHeight;
 
   var prioritetxtgaelement = document.querySelector('.priorite-block-textGa');
   var prioritetxtdoelement = document.querySelector('.priorite-block-textDo');
   var prioritetxtPosition = prioritetxtgaelement.getBoundingClientRect().top; 
 
-  var windowHeight = window.innerHeight;
+  if (prioritetxtPosition < windowHeight) {
+      prioritetxtgaelement.classList.add('animate');
+      prioritetxtdoelement.classList.add('animate');
+    }
 
+  var blockGrisList = document.querySelectorAll('.block-gris');
+
+  blockGrisList.forEach(function(blockGris) {
+      var blockGrisPosition = blockGris.getBoundingClientRect().top;
+
+      if (blockGrisPosition < windowHeight) {
+        blockGris.classList.add('animate');
+      }
+    });
+
+  var gerantelement = document.querySelector('.gerant-element');
+  var gerantelementPosition = gerantelement.getBoundingClientRect().top;
+  
   if (gerantelementPosition < windowHeight) {
     gerantelement.classList.add('animate');
   }
 
-  if (prioritetxtPosition < windowHeight) {
-    prioritetxtgaelement.classList.add('animate');
-    prioritetxtdoelement.classList.add('animate');
+  var presentationTexte1 = document.querySelector('.presentation-texte1');
+  var presentationTexte2 = document.querySelector('.presentation-texte2');
+  var windowHeight = window.innerHeight;
+
+  var presentationTexte1Position = presentationTexte1.getBoundingClientRect().top;
+  var presentationTexte2Position = presentationTexte2.getBoundingClientRect().top;
+
+  if (presentationTexte1Position < windowHeight) {
+    presentationTexte1.classList.add('animate');
   }
 
+  if (presentationTexte2Position < windowHeight) {
+    presentationTexte2.classList.add('animate');
+  }
+
+  var presentationBouttonDevis = document.querySelector('.presentation-boutton-devis');
+
+  var presentationBouttonDevisPosition = presentationBouttonDevis.getBoundingClientRect().top;
+
+  if (presentationBouttonDevisPosition < windowHeight) {
+    presentationBouttonDevis.classList.add('animate');
+  }
+
+  var prioriteTitle = document.querySelector('.priorite-title');
+  var prioriteTitlePosition = prioriteTitle.getBoundingClientRect().top;
+
+  if (prioriteTitlePosition < windowHeight) {
+    prioriteTitle.classList.add('animate');
+  }
+
+  var gerantTitre = document.querySelector('.gerant-title');
+  var gerantTitrePosition = gerantTitre.getBoundingClientRect().top;
+
+  if (gerantTitrePosition < windowHeight) {
+    gerantTitre.classList.add('animate');
+  }
+
+  
 });
+
+
+
+
       
