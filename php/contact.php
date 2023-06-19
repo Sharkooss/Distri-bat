d3caf63a7e541f7ce62585c9f66fc7e9b57143bb
Petre Charlie <charliepetre2004@gmail.com>
2023-06-19 13:16:50 +0200

    contact preskpré

--- php/contact.php
+++ php/contact.php
@@ -12,7 +12,7 @@
 
 
 <header>
-    <?php include 'header.php'; ?>    
+    <?php include 'header.php'; ?>     
 </header>
 
 
@@ -20,31 +20,53 @@
 
 
 <div class="contmom">
+    
     <div class="containercontacttitle">
         <h1>Contact</h1>
     </div>
 
     <div class="containercontact">
 
+        <div class="paracontact responsive"></div>
 
-    <div class="contactgauche">
-        <h2>rénovation et embellissements intérieurs</h2>
-        <h2>Isolation thermique par <br> l'extérieur</h2>
-        <h2>Ravalement de façades</h2>
-    </div>
-    <div class="paracontact"></div>
-    <div class="contactinfo">
-    <div class="coocontainer"><img src="\photo\contact\phone.svg" alt="phone photo" class="contactsvg"> <h2>06 70 04 55 76</h2></div>
-        <div class="coocontainer"><img src="\photo\contact\at.svg" alt="mail photo" class="contactsvg"> <h2>distribatsinistres@ <br> gmail.com</h2></div>
+        <div class="contactgauche">
+            <h2 class="content gauche">rénovation et embellissements intérieurs</h2>
+            <div class="petitseparateur"></div>
+            <h2 class="content milieu">Isolation thermique par l'extérieur</h2>
+            <div class="petitseparateur"></div>
+            <h2 class="content droite">Ravalement de façades</h2>
+        </div>
 
-        <div class="coocontainer"><img src="\photo\contact\location.svg" alt="location photo" class="contactsvg"> <h2>Siège social à Lattes <br> Agence logistique à Poussan</h2></div>
+
+        <div class="paracontact"></div>
+
+
+        <div class="contactinfo">
+
+            <div class="coocontainer un"><img src="\photo\contact\phone.svg" alt="phone photo" class="contactsvg"> <h2 class="coocontent">06 70 04 55 76</h2></div>
+            <div class="petitseparateur"></div>
+            <div class="coocontainer  deux"><img src="\photo\contact\at.svg" alt="mail photo" class="contactsvg"> <h2 class="coocontent">distribatsinistres @gmail.com</h2></div>
+            <div class="petitseparateur"></div>
+            <div class="coocontainer trois"><img src="\photo\contact\location.svg" alt="location photo" class="contactsvg"> <h2 class="coocontent">Siège social à Lattes Agence logistique à Poussan</h2></div>
+        
+        </div>
+
+        
+        <div class="paracontact"></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.4003355269156!2d3.67300007657708!3d43.47311506405905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6bb6565230a9b%3A0x15ab78633134ae21!2sDISTRI-BAT%20SINISTRES!5e0!3m2!1sen!2sfr!4v1685709156392!5m2!1sen!2sfr" width="800" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="locacontact"></iframe>
+        <div class="paracontact responsive"></div>
     </div>
-    <div class="paracontact"></div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.4003355269156!2d3.67300007657708!3d43.47311506405905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6bb6565230a9b%3A0x15ab78633134ae21!2sDISTRI-BAT%20SINISTRES!5e0!3m2!1sen!2sfr!4v1685709156392!5m2!1sen!2sfr" width="800" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="locacontact"></iframe>
-    </div>
+
+    
 
 
     <div class="deviscontainermom">
+
+
+        <div class="deviscontainer">
+            <div class="devis"></div>
+        </div>
+
         <div class="cerificationcontainer">
             <h2 class="devistitre">Demande de devis</h2>
             <div class="separateur"></div>
@@ -53,8 +75,7 @@
             <p class="demandedevistexte">Réalise vos projets et <br> vous accompagne en continu <br> durant les travaux !</p>
             <div class="separateur"></div>
             <div class="labelcontainer">
-                <img src="\photo\Logo\RGE.png" alt="RGE logo" class="labelimg">
-                <img src="\photo\Logo\logo-maif.png" alt="MAIF logo" class="labelimg">
+                <img src="/photo/entreprise/RGE.png" alt="RGE logo" class="labelimg">
             </div>
             <div class="separateur"></div>
             <div class="rscontainer">
@@ -63,9 +84,7 @@
                 <img src="\photo\Logo\linkedin.svg" alt="linkedin" class="rslogo">
             </div>
         </div>
-        <div class="deviscontainer">
-            <div class="devis"></div>
-        </div>
+
     </div>
 
 </div>
