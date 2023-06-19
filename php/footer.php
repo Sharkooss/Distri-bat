a309cd8d3215016bc5fc76cf629206ed6d28ccfd
Sharkooss <lnectoux04@gmail.com>
2023-06-19 12:29:25 +0200

    footer jusqu'au fold

--- php/footer.php
+++ php/footer.php
@@ -1,81 +1,133 @@
-<div class="footer">
 
-    <div class="overflow-contact">
-        <div class="menufootercontact" id="menufootercontact">
-            <div class="footer-title contact-t">
+
+    <div class="footer-wide">
+        <div class="overflow-contact">
+            <div class="menufootercontact" id="menufootercontact">
+                <div class="footer-title contact-t">
+                    <h2>Nous Contacter</h2>
+                    <div class="footer-underline contact-u"></div>
+                </div>
+                <div class="footer-contact-description">
+                    <div class="footer-contact-tel">
+                        <img src="../photo/logo/phone.svg" alt="" class="tel-img">
+                        <div class="tel-text">06 70 04 55 76</div>
+                    </div>
+                    <div class="footer-contact-mail">
+                        <img src="../photo/logo/at.svg" alt="" class="tel-img">
+                        <div class="tel-text">distribatsinistres@gmail.com</div>
+                    </div>
+                    <div class="footer-contact-reseau">
+                        <img src="../photo/logo/instagram.svg" alt="" class="tel-img">
+                        <img src="../photo/logo/facebook.svg" alt="" class="tel-img">
+                        <img src="../photo/logo/linkedin.svg" alt="" class="tel-img">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="overflow-locaux" id>
+            <div class="menufooterlocaux" id="menufooterlocaux">
+                <div class="footer-title locaux-t">
+                    <h2>Nos Locaux</h2>
+                    <div class="footer-underline locaux-u"></div>
+                </div>
+                <div class="siege-social">
+
+                    <div class="siege-social-top">
+                        <div class="siege-social-topleft">
+                            <img src="../photo/logo/home.svg" alt="" class="home-img">
+                            <div class="siege-social-txt">
+                                <h2>Siège social</h2>
+                                <p>16 rue des Érables, 34970 Lattes</p>
+                            </div>
+                        </div>
+
+                        <div class="siege-social-topright">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d180.68299699336902!2d3.912706055231308!3d43.56638436233913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6baac22356ce1%3A0xf1a3ed331e75ad12!2s16%20Rue%20des%20%C3%89rables%2C%2034970%20Lattes!5e0!3m2!1sfr!2sfr!4v1685949066065!5m2!1sfr!2sfr"style="border:1px solid #373B4D ;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
+                        </div>                        
+                    </div>
+                    <div class="siege-social-bot">
+
+                        <div class="siege-social-botleft">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11581.554335626044!2d3.672957164001465!3d43.47336034789388!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6bb6565230a9b%3A0x15ab78633134ae21!2sDISTRI-BAT%20SINISTRES!5e0!3m2!1sfr!2sfr!4v1685949255076!5m2!1sfr!2sfr" style="border:1px solid #373B4D ;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
+                        </div>
+                        <div class="siege-social-botright">
+                            <img src="../photo/logo/home.svg" alt="" class="home-img">
+                            <div class="siege-social-txt">
+                                <h2>Agence logistique</h2>
+                                <p>304 Av. des Lauriers, 34560 Poussan</p>
+                            </div>
+                        </div>
+
+                    </div>
+                            
+                </div>
+                <div class="agence-logistique"></div>
+            </div>
+        </div>
+
+        <div class="overflow-devis">
+            <div class="menufooterdevis" id="menufooterdevis">
+                <div class="footer-title devis-t">
+                    <h2>politique de confidentialité</h2>
+                    <div class="footer-underline devis-u"></div>
+                </div>
+                <div class="mentions-legales">
+                    <h2>Mentions légales</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-responsive">
+        <div class="left-footer">
+            <div class="footer-title-responsive">
                 <h2>Nous Contacter</h2>
-                <div class="footer-underline contact-u"></div>
+                <div class="underline"></div>
             </div>
-            <div class="footer-contact-description">
-                <div class="footer-contact-tel">
-                    <img src="../photo/logo/phone.svg" alt="" class="tel-img">
-                    <div class="tel-text">06 70 04 55 76</div>
+            <div class="left-contact">
+                <div class="contact-item">
+                    <img src="../photo/logo/phone.svg" alt="" class="left-tel-img">
+                    <div class="left-tel">06 70 04 55 76</div>
                 </div>
-                <div class="footer-contact-mail">
-                    <img src="../photo/logo/at.svg" alt="" class="tel-img">
-                    <div class="tel-text">distribatsinistres@gmail.com</div>
+                <div class="contact-item">
+                    <img src="../photo/logo/at.svg" alt="" class="left-tel-img">
+                    <div class="left-mail">distribatsinistres@gmail.com</div>
                 </div>
-                <div class="footer-contact-reseau">
-                    <img src="../photo/logo/instagram.svg" alt="" class="tel-img">
-                    <img src="../photo/logo/facebook.svg" alt="" class="tel-img">
-                    <img src="../photo/logo/linkedin.svg" alt="" class="tel-img">
-                </div>
+            </div>
+
+            <div class="left-reseau">
+                <img src="../photo/logo/instagram.svg" alt="" class="left-tel-img">
+                <img src="../photo/logo/facebook.svg" alt="" class="left-tel-img">
+                <img src="../photo/logo/linkedin.svg" alt="" class="left-tel-img">
             </div>
         </div>
-    </div>
-
-    <div class="overflow-locaux" id>
-        <div class="menufooterlocaux" id="menufooterlocaux">
-            <div class="footer-title locaux-t">
-                <h2>Nos Locaux</h2>
-                <div class="footer-underline locaux-u"></div>
+        <div class="middle-footer">
+            <div class="footer-title-responsive">
+                <h2>Siège social</h2>
+                <div class="underline"></div>
             </div>
-            <div class="siege-social">
-                
-                <div class="siege-social-top">
-                    <div class="siege-social-topleft">
-                        <img src="../photo/logo/home.svg" alt="" class="home-img">
-                        <div class="siege-social-txt">
-                            <h2>Siège social</h2>
-                            <p>16 rue des Érables, 34970 Lattes</p>
-                        </div>
-                    </div>
-
-                    <div class="siege-social-topright">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d180.68299699336902!2d3.912706055231308!3d43.56638436233913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6baac22356ce1%3A0xf1a3ed331e75ad12!2s16%20Rue%20des%20%C3%89rables%2C%2034970%20Lattes!5e0!3m2!1sfr!2sfr!4v1685949066065!5m2!1sfr!2sfr"style="border:1px solid #373B4D ;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
-                    </div>                        
+            <div class="middle-siege">
+                <img src="../photo/logo/homeB.svg" alt="" class="middle-home-img">
+                <div class="middle-texte">
+                    <h2>Siège social</h2>
+                    <p>16 rue des Érables, 34970 Lattes</p>
                 </div>
-                <div class="siege-social-bot">
-
-                    <div class="siege-social-botleft">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11581.554335626044!2d3.672957164001465!3d43.47336034789388!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12b6bb6565230a9b%3A0x15ab78633134ae21!2sDISTRI-BAT%20SINISTRES!5e0!3m2!1sfr!2sfr!4v1685949255076!5m2!1sfr!2sfr" style="border:1px solid #373B4D ;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
-                    </div>
-                    <div class="siege-social-botright">
-                        <img src="../photo/logo/home.svg" alt="" class="home-img">
-                        <div class="siege-social-txt">
-                            <h2>Agence logistique</h2>
-                            <p>304 Av. des Lauriers, 34560 Poussan</p>
-                        </div>
-                    </div>
-
-                </div>
-                
             </div>
-            <div class="agence-logistique"></div>
+            <div class="middle-siege">
+                <img src="../photo/logo/homeB.svg" alt="" class="middle-home-img">
+                <div class="middle-texte">
+                    <h2>Agence logistique</h2>
+                    <p>304 Av. des Lauriers, 34560 Poussan</p>
+                </div>
+            </div>                  
         </div>
-    </div>
-
-    <div class="overflow-devis">
-        <div class="menufooterdevis" id="menufooterdevis">
-            <div class="footer-title devis-t">
+        <div class="right-footer">
+            <div class="footer-title-responsive">
                 <h2>politique de confidentialité</h2>
-                <div class="footer-underline devis-u"></div>
+                <div class="underline"></div>
             </div>
-            <div class="mentions-legales">
+            <div class="right-legales">
                 <h2>Mentions légales</h2>
             </div>
         </div>
     </div>
-
-</div>
-
