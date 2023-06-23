acb7d5e533fe942060bcb2c4a3d8c142270b3ed4
Sharkooss <lnectoux04@gmail.com>
2023-06-23 08:55:23 +0200

    all fixes

--- js/header.js
+++ js/header.js
@@ -1,39 +1,43 @@
 window.addEventListener('DOMContentLoaded', function() {
-    var navInfo = document.querySelector('.nav-info');
-    var navLogo = document.querySelector('.nav-logo img');
-    var navMenu = document.querySelector('.nav-menu');
+  var navInfo = document.querySelector('.nav-info');
+  var navLogo = document.querySelector('.nav-logo img');
+  var navMenu = document.querySelector('.nav-menu');
 
-    var isAtTop = true;
-    var logoText = document.createElement('span');
-    logoText.classList.add('logo-text');
-    logoText.textContent = 'DISTRI-BAT';
+  var isAtTop = true;
+  var logoText = document.createElement('span');
+  logoText.classList.add('logo-text');
+  logoText.textContent = 'DISTRI-BAT';
+  logoText.href = '/index.php';
 
-    function handleScroll() {
-        if ((window.pageYOffset > 0 && isAtTop) || window.innerWidth < 1501) {
-            isAtTop = false;
+  function handleScroll() {
+    if ((window.pageYOffset > 0 && isAtTop) || window.innerWidth < 1501) {
+      isAtTop = false;
 
-            navInfo.classList.add('scrolled-info');
-            navInfo.appendChild(logoText);
-            navMenu.classList.add('scrolled-menu');
-        } else if (window.pageYOffset === 0 && !isAtTop && window.innerWidth >= 1500) {
-            isAtTop = true;
+      navInfo.classList.add('scrolled-info');
+      navInfo.appendChild(logoText);
+      navMenu.classList.add('scrolled-menu');
+    } else if (window.pageYOffset === 0 && !isAtTop && window.innerWidth >= 1500) {
+      isAtTop = true;
 
-            navInfo.classList.remove('scrolled-info');
-            navInfo.removeChild(logoText);
-            navMenu.classList.remove('scrolled-menu');
-        }
+      navInfo.classList.remove('scrolled-info');
+      navInfo.removeChild(logoText);
+      navMenu.classList.remove('scrolled-menu');
     }
+  }
 
-    // Vérifier la taille de l'écran lors du chargement de la page
-    if (window.innerWidth < 1500) {
-        navInfo.classList.add('scrolled-info');
-        navInfo.appendChild(logoText);
-        navMenu.classList.add('scrolled-menu');
-    }
+  // Vérifier la taille de l'écran lors du chargement de la page
+  if (window.innerWidth < 1500) {
+    navInfo.classList.add('scrolled-info');
+    navInfo.appendChild(logoText);
+    navMenu.classList.add('scrolled-menu');
+  }
 
-    window.addEventListener('scroll', handleScroll);
-    window.addEventListener('resize', handleScroll);
+  window.addEventListener('scroll', handleScroll);
+  window.addEventListener('resize', handleScroll);
 });
+
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var navToggler = document.querySelector('.nav-toggler');
@@ -56,5 +60,34 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', removeToggledClass);
   });
   
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Récupérer le chemin de la page actuelle
+  var path = window.location.pathname;
+
+  // Sélectionner tous les liens dans la barre de navigation
+  var navLinks = document.querySelectorAll(".nav-menu a");
+
+  // Parcourir tous les liens et ajouter la classe "blue" à celui correspondant au chemin de la page
+  for (var i = 0; i < navLinks.length; i++) {
+    var link = navLinks[i];
+
+    // Vérifier si le chemin de la page correspond à l'attribut "href" du lien
+    if (link.getAttribute("href") === path) {
+      link.classList.add("blue");
+      break; // Arrêter la boucle après avoir trouvé le lien correspondant
+    }
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var navInfo = document.querySelector(".nav-info");
+
+  // Ajouter la classe "animation-enabled" après un délai de 1 seconde
+  setTimeout(function() {
+    navInfo.classList.add("animation-enabled");
+  }, 1000);
+});
+
 
 
--- php/header.php
+++ php/header.php
@@ -1,14 +1,14 @@
 <div class="nav-main">
     <div class="nav-info">
         <div class="nav-logo">
-            <img src="/photo/Logo/logo-complet-blanc.jpg" alt="logo distribat sur fond blanc">
+            <a href="/index.php"><img src="/photo/Logo/logo-complet-blanc.jpg" alt="logo distribat sur fond blanc"></a>
         </div>
         <div class="nav-menu">
             <a href="/index.php" class="nav-acceuil">Accueil</a>
             <a href="/php/entreprise.php" class="nav-entreprise">L'Entreprise</a>
-            <a href="\php\activite.php" class="nav-activite">Nos Activités</a>
+            <a href="/php/activite.php" class="nav-activite">Nos Activités</a>
             <a href="/php/realisation.php" class="nav-realisation">Nos Réalisations</a>
-            <a href="\php\contact.php" class="nav-contact">Contact</a>
+            <a href="/php/contact.php" class="nav-contact">Contact</a>
         </div>
     </div>
     <div class="nav-toggler"><img src="/photo/logo/bars-solid.svg" alt=""></div>
